b30c261684be322a5339eecedb7061576a226db7
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-10-28 01:09:50 -0300

    test: :white_check_mark: add character tests

--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -21,3 +21,35 @@ describe('GET: /character/:n', () => {
     expect(response.body).toHaveLength(2)
   })
 })
+
+describe('POST: /character', () => {
+  const validCharacter = {
+    name: 'Rick',
+    status: 'Alive',
+    species: 'Human',
+    origin: 'Earth'
+  }
+
+  test('should return 400 if there is a missing field', async () => {
+    const invalidCharacters = [
+      { ...validCharacter, name: undefined },
+      { ...validCharacter, status: undefined },
+      { ...validCharacter, species: undefined },
+      { ...validCharacter, origin: undefined }
+    ]
+
+    for (const character of invalidCharacters) {
+      const response = await request(app).post('/character').send(character)
+      expect(response.statusCode).toBe(400)
+    }
+  })
+
+  test('given a valid character, should return the character created', async () => {
+    const response = await request(app).post('/character').send(validCharacter)
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toHaveProperty('name', 'Rick')
+    expect(response.body).toHaveProperty('status', 'Alive')
+    expect(response.body).toHaveProperty('species', 'Human')
+    expect(response.body).toHaveProperty('origin', 'Earth')
+  })
+})
