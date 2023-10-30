af7d15a8003bce0429e822d53532ae2000e0944a
Tomás Opazo <tomas.opazoasd@gmail.com>
2023-10-29 22:12:38 -0300

    refactor: :recycle: refactor swagger docs

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,18 +1,8 @@
-import swaggerJSDoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
-
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: { title: 'Rick and Morty API - Pinflag backend challenge', version: '1.0.0' }
-  },
-  apis: ['./src/routes/*.js']
-}
-
-const swaggerSpec = swaggerJSDoc(options)
+import v1 from '../docs/v1.json'
 
 const swaggerDocs = (app) => {
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(v1))
 }
 
 export default swaggerDocs
